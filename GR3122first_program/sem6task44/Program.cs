6fdf0fc3743684e92f1b264fa98ced88fe7ac5c7
Olga Enygina <olga.enygina@bk.ru>
2022-09-26 09:30:10 +0300

    add solution for tak 44 46 47 48 49 51

--- GR3122first_program/sem6task44/Program.cs
+++ GR3122first_program/sem6task44/Program.cs
@@ -3,106 +3,54 @@
 Фибоначчи. Первые два числа Фибоначчи: 0 и 1*/
 
 
-// // Создание словаря.
-// Dictionary<int, double> fiboDict = new Dictionary<int, double>
-// {
-//     { 0, 1 },
-//     { 1, 1 }
-// };
+// Создание словаря.
+Dictionary<int, double> fiboDict = new Dictionary<int, double>
+{
+    { 0, 1 },
+    { 1, 1 }
+};
 
-// // Фибоначи рекурсия.
-// double FibonachiRec(int numb, Dictionary<int, double> fiboDict)
-// {
-//     if (fiboDict.ContainsKey(numb))
-//         return fiboDict[numb];
+// Фибоначи рекурсия.
+double FibonachiRec(int numb, Dictionary<int, double> fiboDict)
+{
+    if (fiboDict.ContainsKey(numb))
+        return fiboDict[numb];
 
-//     else
-//     {
-//         double fiboSum = FibonachiRec(numb - 2, fiboDict) + FibonachiRec(numb - 1, fiboDict);
-//         fiboDict.Add(numb, fiboSum);
-//         return fiboSum;
-//     }
-// }
+    else
+    {
+        double fiboSum = FibonachiRec(numb - 2, fiboDict) + FibonachiRec(numb - 1, fiboDict);
+        fiboDict.Add(numb, fiboSum);
+        return fiboSum;
+    }
+}
 
-// // Фибоначи цикл
-// double FibonachiСycle(int numb)
-// {
-//     double fiboPrev = 0;
-//     double fibo = 1;
-//     for (int i = 0; i < numb; i++)
-//     {
-//         double tmp = fibo;
-//         fibo += fiboPrev;
-//         fiboPrev = tmp;
-//     }
-//     return fiboPrev;
-// }
-
-
-// int fiboNumb = 50;
+// Фибоначи цикл
+double FibonachiСycle(int numb)
+{
+    double fiboPrev = 0;
+    double fibo = 1;
+    for (int i = 0; i < numb; i++)
+    {
+        double tmp = fibo;
+        fibo += fiboPrev;
+        fiboPrev = tmp;
+    }
+    return fiboPrev;
+}
 
 
-// for (int i = 0; i < fiboNumb; i++)
-// {
-//     // Console.WriteLine($"{i} --> {FibonachiRec(i, fiboDict)}");
-//     Console.WriteLine(FibonachiСycle(i)+" ");
-// }
+int fiboNumb = 50;
 
 
-
-// // int fiboNumb = 3;
-// // Метод считывания данных пользователя
-// int ReadData(string line)
-// {
-//     // Выводим сообщение
-//     Console.Write(line);
-//     // Считываем число
-//     int number = int.Parse(Console.ReadLine() ?? "0");
-//     // Возвращаем значение
-//     return number;
-// }
-
-// // Метод, печатает одномерный массив
-// void Print1DArray(int[] arr)
-// {
-//     string arrString = "";
-
-//     for (int i = 0; i < arr.Length - 1; i++)
-//     {
-//         arrString += arr[i] + ", ";
-//     }
-
-//     arrString += arr[arr.Length - 1];
-//     Console.WriteLine(arrString);
-// }
-
-// int[] Fibonacci(int number)
-// {
-//     int[] fibonacciArr = new int[number];
-//     fibonacciArr[1] = 1;
-
-//     for (int i = 2; i < number; i++)
-//     {
-//         fibonacciArr[i] = fibonacciArr[i - 2] + fibonacciArr[i - 1];
-//     }
-
-//     return fibonacciArr;
-// }
-
-// int inputNumber = ReadData("Введите длину последовательности: ");
-
-// Console.WriteLine($"Последовательность Фибоначчи длиной {inputNumber}:");
-// Print1DArray(Fibonacci(inputNumber));
+for (int i = 0; i < fiboNumb; i++)
+{
+    // Console.WriteLine($"{i} --> {FibonachiRec(i, fiboDict)}");
+    Console.WriteLine(FibonachiСycle(i)+" ");
+}
 
 
 
-// for (int i = 0; i < fiboNumb; i++)
-// {
-//     //Console.WriteLine($"{i} --> {FibonachiRec(i, fiboDict)}");
-//     Console.Write(FibonachiСycle(i)+" ");
-// }
-
-
+// int fiboNumb = 3;
 // Метод считывания данных пользователя
 int ReadData(string line)
 {
@@ -113,6 +61,47 @@ int ReadData(string line)
     // Возвращаем значение
     return number;
 }
+
+// Метод, печатает одномерный массив
+void Print1DArray(int[] arr)
+{
+    string arrString = "";
+
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        arrString += arr[i] + ", ";
+    }
+
+    arrString += arr[arr.Length - 1];
+    Console.WriteLine(arrString);
+}
+
+int[] Fibonacci(int number)
+{
+    int[] fibonacciArr = new int[number];
+    fibonacciArr[1] = 1;
+
+    for (int i = 2; i < number; i++)
+    {
+        fibonacciArr[i] = fibonacciArr[i - 2] + fibonacciArr[i - 1];
+    }
+
+    return fibonacciArr;
+}
+
+int inputNumber = ReadData("Введите длину последовательности: ");
+
+Console.WriteLine($"Последовательность Фибоначчи длиной {inputNumber}:");
+Print1DArray(Fibonacci(inputNumber));
+
+
+
+for (int i = 0; i < fiboNumb; i++)
+{
+    //Console.WriteLine($"{i} --> {FibonachiRec(i, fiboDict)}");
+    Console.Write(FibonachiСycle(i)+" ");
+}
+
 
 // Метод, принимает строку, выводит в консоль
 void PrintResult(string prefix, string line)
@@ -136,8 +125,8 @@ string FibNum(int num)
     return res;
 }
 
-// int numFib = ReadData("Введите количество чисел фибоначчи: ");
-// string line = FibNum(numFib);
-// PrintResult("Числа фибоначчи: ", line);
+int numFib = ReadData("Введите количество чисел фибоначчи: ");
+string line = FibNum(numFib);
+PrintResult("Числа фибоначчи: ", line);
 
 PrintResult("Числа фибоначчи: ", FibNum(ReadData("Введите количество чисел фибоначчи: ")));
---
+++ GR3122first_program/sem7task49/Program.cs
@@ -0,0 +1,132 @@
+﻿/*Задача №49
+Задайте двумерный массив. Найдите элементы, у
+которых оба индекса чётные, и замените эти
+элементы на их квадраты.
+Например:
+Например, изначально массив выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Новый массив будет выглядеть вот так:
+1 4 7 2
+5 81 2 9
+8 4 2 4*/
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+
+// Заполняем массив 
+int[,] Fill2DArray(int[,] matr,int min, int max)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matr;
+}
+
+// Печать двумерного массива
+void Print2DArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] UpDate2DArray(int[,] arr2D)
+{
+    for (int i = 0; i < arr2D.GetLength(0); i = i + 2)
+    {
+        for (int j = 0; j < arr2D.GetLength(1); j = j + 2)
+        {
+            {
+                arr2D[i, j] = arr2D[i, j] * arr2D[i, j];
+            }
+        }
+
+
+    }
+    return arr2D;
+}
+int row = ReadData("Введите количество столбцов: ");
+int column = ReadData("Введите количество строк: ");
+int[,] arr2D = new int[row, column];
+
+Fill2DArray(arr2D, 10, 99);
+Print2DArray(arr2D);
+UpDate2DArray(arr2D);
+
+Console.WriteLine();
+Print2DArray(arr2D);
+
+
+// Печать 2D массива.
+void Print2DArr(int[,] arr, string message = "Массив: ")
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Генерация случайного двумерного массива.
+int[,] Gen2DArr(int arrLen, int arrDepth, int arrMin, int arrMax)
+{
+    int[,] arr = new int[arrLen, arrDepth];
+    Random rnd = new Random();
+
+    if (arrMin > arrMax)
+    {
+        int temp = arrMax;
+        arrMax = arrMin;
+        arrMin = temp;
+    }
+
+    for (int i = 0; i < arrLen; i++)
+    {
+        for (int j = 0; j < arrDepth; j++)
+        {
+            arr[i, j] = rnd.Next(arrMin, arrMax);
+        }
+    }
+    return arr;
+}
+//
+int[,] EvenToSquere(int[,] arr)
+{
+    for (int i = 1; i < arr.GetLength(0); i+=2)
+    {
+        for (int j = 1; j < arr.GetLength(1); j+=2)
+        {
+            arr[i, j] *= arr[i, j];
+        }        
+    }
+    return arr;
+}
+
+int[,] arr = Gen2DArr(10, 10, 0, 20);
+Print2DArr(arr);
+Console.WriteLine();
+Console.WriteLine();
+Print2DArr(EvenToSquere(arr));
